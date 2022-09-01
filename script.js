c4619c652b25921b2d654aace07575078e785106
Mtomchik <matthew.tomchik@yahoo.com>
2022-08-31 22:11:17 -0600

    reviewed and clarified code after office hours

--- script.js
+++ script.js
@@ -1,89 +1,79 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numerals =["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","@","#","$","%","^","&","*"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
 function randomInt(min, max) {
 if (!max) {
   max = min
   min = 0
 }
 var rand= Math.random()
-return Math.floor(min*(1 - rand) + rand*max)
+return Math.floor(min*(1 - rand) + rand*max);
 }
 
 function getRandomitem(list) {
-  return list [randomInt(list.length)]
+  return list [randomInt(list.length)];
 }
 
 
 function generatePassword() {
 
-  var passwordSize = window.prompt("How many characters long would you like your password to be (8-128)?")
+  var passSize = window.prompt("How many characters long would you like your password to be (8-128)?");
 
-  var passwordLength = parseInt(passwordSize)
+  var passLength = parseInt(passSize);
 
-  if (isNaN(passwordLength)) {
-    window.alert("That's not a number!")
+  if (isNaN(passLength)) {
+    window.alert("Enter a number please!");
     return
   }
 
-  if (passwordLength < 8) {
-    window.alert("Must be at least 8 characters")
+  if (passLength < 8) {
+    window.alert("Must be at least 8 characters");
     return
   }
 
-  if (passwordLength > 128) {
-    window.alert("Must be no more than 128 characters!")
+  if (passLength > 128) {
+    window.alert("Must be no more than 128 characters!");
     return
   }
   
-  
-  var number= window.confirm("Would you like your password to contain numbers?")
-  var special = window.confirm("Would you like your password to contain special characters?")
-  var lower = window.confirm ("Would you like your password to include lowercase letters?")
-  var upper = window.confirm ("Would you like your password to include uppercase letters?")
+  var number= window.confirm("Would you like your password to contain numbers?");
+  var special = window.confirm("Would you like your password to contain special characters?");
+  var lower = window.confirm ("Would you like your password to include lowercase letters?");
+  var upper = window.confirm ("Would you like your password to include uppercase letters?");
 
-  
-  var numerals =["0","1","2","3","4","5","6","7","8","9"]
-  var specialChar = ["!","@","#","$","%","^","&","*"]
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  
   var selection = []
-  /*can also be written
-  var upperCase =[]
-  for (var i=0; i< lowerCase.length; i++) {
-    upperCase[i] =lowerCase[i].toUpperCase()
-  }
-  */
-
-//this is our "shopping cart" for what we want in our password
 
 if (number === true) {
-  selection.push(numerals)
-
+  selection.push(numerals);
 }
 
 if (special === true) {
-  selection.push(specialChar)
+  selection.push(specialChar);
 }
 
 if (lower === true) {
-  selection.push(lowerCase)
+  selection.push(lowerCase);
 }
 
 if (upper === true) {
-selection.push(upperCase)
+selection.push(upperCase);
   }
 
 if (selection.length === 0){
-  selection.push(upperCase)
+  selection.push(upperCase);
   }
 
 var generatedPassword = ""
 
-for (var i = 0; i < passwordLength; i++) {
-var randomList = getRandomitem(selection)
-var randomChar = getRandomitem(randomList)
+for (var i = 0; i < passLength; i++) {
+var randomList = getRandomitem(selection);
+var randomChar = getRandomitem(randomList);
 generatedPassword += randomChar
   }
   return generatedPassword
